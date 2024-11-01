4d3b76a765acff6b4f77ada0b6b32afc602a5e07
pgyseven <pgyseven@naver.com>
2024-11-01 15:44:09 +0900

    feat: 클릭 이벤트 처리 기초

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,32 +1,23 @@
 import React from 'react';
-function Body({name, age, addr, hobbies}) {
+function Body() {
     
+    function handleOnClick(e) {
+        console.log(e.target);
+        if (e.target.name === 'A') {  
+            alert('Button A clicked!');
+        }else if (e.target.name === 'B') {
+            alert('Button B clicked!');
+        } 
+    }
     return (
-        <>
-        <div style={{backgroundColor:'green'}}>
-            이름 : {name}
-        </div>
-        <div style={{backgroundColor:'gold'}}>
-            나이 : {age}
-        </div>
-        <div style={{backgroundColor:'orange'}}>
-            주소 : {addr}
-        </div>
+        <div className='body'>
+            <button name="A" onClick={handleOnClick}>클릭A</button>
+            <button name="B" onClick={handleOnClick}>클릭B</button>
 
-        <div>
-            {name}학생의 취미는 {hobbies.length}가지 입니다.
         </div>
-    </>
     );
 
     
 }
-//부모 컴포넌트에서 넘겨온 props 중에서 값이 제대로 전달되지 않는 속성이 있다면 에러가 나는데,
-// 값이 전달되지 않더라도 기본값을 줄 수 있다.
-Body.defaultProps = {
-    hobbies : []
-};
-export default Body;
 
-// export : 기본적으로 변수, 함수, 상수, 객체등을 내보내기 할 때 사용
-// export default : 하나의 js 파일내에 하나의 컴포넌트가 있고, 단 하나만 내보낼때
+export default Body;
