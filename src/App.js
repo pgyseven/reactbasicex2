49fbdccecbbdf3016a02c05c105f6ce3f620f498
pgyseven <pgyseven@naver.com>
2024-11-01 15:25:48 +0900

    feat: props 기본 사용법 익히기

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import Body from "./component/Body.js";
 import Footer from "./component/Footer.js";
 
 function App() {
+  const stuInfo = {
+    name: "John Park",
+    age: 25,
+    addr: "서울시",
+    //hobbies: ['낮잠', '코딩', '운동', '영화보기']
+  }
+
   return (
     <div className="App">
       <Header />
-      <Body />
+      {/* <Body name={stuInfo.Name} age={stuInfo.Age} addr={stuInfo.addr} /> */}
+      <Body {...stuInfo} />
+
       <Footer />
     </div>
   );
--- src/component/Body.js
+++ src/component/Body.js
@@ -1,12 +1,31 @@
 import React from 'react';
-function Body() {
-    const num = 20;
-    if (num === 0) {
-        return <div style={{backgroundColor: 'yellow', color: 'gray'}}>{num} is even</div>;
-  }else{
-    return <div style={{backgroundColor: 'yellow', color: 'gray'}}>{num} is odd</div>;
-  }
+function Body({name, age, addr, hobbies}) {
+    
+    return (
+        <>
+        <div style={{backgroundColor:'green'}}>
+            이름 : {name}
+        </div>
+        <div style={{backgroundColor:'gold'}}>
+            나이 : {age}
+        </div>
+        <div style={{backgroundColor:'orange'}}>
+            주소 : {addr}
+        </div>
+
+        <div>
+            {name}학생의 취미는 {hobbies.length}가지 입니다.
+        </div>
+    </>
+    );
+
+    
 }
+//부모 컴포넌트에서 넘겨온 props 중에서 값이 제대로 전달되지 않는 속성이 있다면 에러가 나는데,
+// 값이 전달되지 않더라도 기본값을 줄 수 있다.
+Body.defaultProps = {
+    hobbies : []
+};
 export default Body;
 
 // export : 기본적으로 변수, 함수, 상수, 객체등을 내보내기 할 때 사용
